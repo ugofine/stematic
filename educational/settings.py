78018611f81509b67a3946e350eef1a4710dd4f2
Debbie <62076688+ugofine@users.noreply.github.com>
2021-07-13 12:02:47 +0100

    changes to templates

--- educational/settings.py
+++ educational/settings.py
@@ -66,7 +66,7 @@ ROOT_URLCONF = 'educational.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['Templates'],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
